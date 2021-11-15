be6df813da8a71917a897c3f763065d9981b1e2e
KevinMwita7 <kevinmwita7@gmail.com>
2021-11-15 16:07:39 +0300

    update image-banner.liquid

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -16,21 +16,19 @@ showSlides();
 function showSlides(event) {
   var i;
   var slides = document.getElementsByClassName("banner-slide");
-  if(slides.length > 1) {
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    var currentSlide = slideIndex - 1;
-    slides[currentSlide].style.display = "block";
-    setActiveDot(currentSlide);
-    // Change image every 5 seconds
-    setTimeout(showSlides, 5000);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  var currentSlide = slideIndex - 1;
+  slides[currentSlide].style.display = "block";
+  setActiveDot(currentSlide);
+  // Change image every 5 seconds
+  setTimeout(showSlides, 5000);
 }
 
-if (Shopify.designMode) {
+if(Shopify.designMode) {
   document.addEventListener("shopify:section:load", function(event) {
     slideIndex = 0;
   });
