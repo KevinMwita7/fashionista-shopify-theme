ef2f81774f329bab5a3961562c68d83a72568c2d
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-09 13:02:39 +0300

    update banner-slider.js

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -1,6 +1,33 @@
+function LiveRegion( Splide ) {
+  let liveRegion;
+
+  function mount() {
+    liveRegion = document.createElement( 'div' );
+    liveRegion.setAttribute( 'aria-live', 'polite' );
+    liveRegion.setAttribute( 'aria-atomic', 'true' );
+    liveRegion.classList.add( 'visually-hidden' );
+
+    Splide.root.appendChild( liveRegion );
+    Splide.on( 'moved', onMoved );
+  }
+
+  function onMoved() {
+    liveRegion.textContent = `Slide ${ Splide.index + 1 } of ${ Splide.length }`;
+  }
+
+  function destroy() {
+    Splide.root.removeChild( liveRegion );
+  }
+
+  return {
+    mount,
+    destroy,
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var splide = new Splide('.splide');
-  splide.mount();
+  splide.mount({ LiveRegion });
 
   if (Shopify.designMode) {
     document.addEventListener('shopify:block:select', function({ detail }) {
@@ -11,6 +38,8 @@ document.addEventListener('DOMContentLoaded', function() {
         splide.go(0);
       } else if(slide2 && slide2.firstElementChild && JSON.parse(slide2.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
         splide.go(1);
+      } else if(slide3 && slide3.firstElementChild && JSON.parse(slide3.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
+        splide.go(2);
       }
     });
   }  
