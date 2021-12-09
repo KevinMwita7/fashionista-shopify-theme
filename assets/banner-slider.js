3e6b9390f8e9d74b916841aafcc366069ce3bba0
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-09 12:55:36 +0300

    update banner-slide.js

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -1,51 +1,17 @@
-function LiveRegion( Splide ) {
-  let liveRegion;
+document.addEventListener('DOMContentLoaded', function() {
+  var splide = new Splide('.splide');
+  splide.mount();
 
-  function mount() {
-    liveRegion = document.createElement( 'div' );
-    liveRegion.setAttribute( 'aria-live', 'polite' );
-    liveRegion.setAttribute( 'aria-atomic', 'true' );
-    liveRegion.classList.add( 'visually-hidden' );
-
-    Splide.root.appendChild( liveRegion );
-    Splide.on( 'moved', onMoved );
-  }
-
-  function onMoved() {
-    liveRegion.textContent = `Slide ${ Splide.index + 1 } of ${ Splide.length }`;
-  }
-
-  function destroy() {
-    Splide.root.removeChild( liveRegion );
-  }
-
-  return {
-    mount,
-    destroy,
-  }
-}
-
-if (Shopify.designMode) {
-  document.addEventListener('shopify:section:load', function() {
-    console.log("loaded");
-    var splide = new Splide('.splide');
-    splide.mount({ LiveRegion });
-  });
-
-  document.addEventListener('shopify:block:select', function({ detail }) {
-    var splide = new Splide('.splide' );
-    let slide1 = document.getElementById('splide01-slide01');
-    let slide2 = document.getElementById('splide01-slide02');
-    let slide3 = document.getElementById('splide01-slide03');
-    if(slide1 && slide1.firstElementChild && JSON.parse(slide1.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
-      splide.go(0);
-    } else if(slide2 && slide2.firstElementChild && JSON.parse(slide2.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
-      splide.go(1);
-    }
-  });
-} else {
-  document.addEventListener('DOMContentLoaded', function() {
-    var splide = new Splide('.splide');
-    splide.mount({ LiveRegion });
-  });
-}
+  if (Shopify.designMode) {
+    document.addEventListener('shopify:block:select', function({ detail }) {
+      let slide1 = document.getElementById('splide01-slide01');
+      let slide2 = document.getElementById('splide01-slide02');
+      let slide3 = document.getElementById('splide01-slide03');
+      if(slide1 && slide1.firstElementChild && JSON.parse(slide1.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
+        splide.go(0);
+      } else if(slide2 && slide2.firstElementChild && JSON.parse(slide2.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
+        splide.go(1);
+      }
+    });
+  }  
+});
