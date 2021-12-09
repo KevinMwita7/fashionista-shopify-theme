1210d8f266c59fae41b799041dfd46fa27ddf117
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-09 10:49:14 +0300

    update image-banner.liquid

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -40,7 +40,7 @@ if (Shopify.designMode) {
     console.log(detail);
     let blocks = document.querySelectorAll('[data-shopify-editor-block]');
     let slides = blocks.forEach(block => {
-      console.log(block.dataset["shopify-editor-block"]);
+      console.log(block, block.dataset);
     })
     // console.log(document.querySelectorAll('[data-shopify-editor-block]'));
   });
