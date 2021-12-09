a4ff18e06df01837ec1c99a715bb09775c1183c4
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-09 12:39:23 +0300

    update banner-slide.js

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -37,13 +37,16 @@ if (Shopify.designMode) {
   });
 
   document.addEventListener( 'shopify:block:select', function({ detail }) {
+    const splide = new Splide( '.splide' );
     let slide1 = document.getElementById("splide01-slide01");
     let slide2 = document.getElementById("splide01-slide02");
     let slide3 = document.getElementById("splide01-slide03");
     if(slide1 && slide1.firstElementChild && JSON.parse(slide1.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
-      console.log(0);
-    } if(slide2 && slide2.firstElementChild && JSON.parse(slide2.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
-      console.log(1);
+      console.log(0)
+      splide.go(0);
+    } else if(slide2 && slide2.firstElementChild && JSON.parse(slide2.firstElementChild.dataset.shopifyEditorBlock).id === detail.blockId) {
+      console.log(1)
+      splide.go(1);
     }
   });
 }
