3429bf4a9c771bec1d318f2b951354dfa1768337
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-15 20:27:56 +0300

    update featured-collection-slider.js

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,5 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', function() {
+    let splides = document.getElementsByClassName("splide__slide");
+
     var splide = new Splide( '.featured-collection-splide', {
+        arrows: splides.length > 4,
         rewind: true,
         perPage: screen.width >= 768 ? 4 : 2
     } );
