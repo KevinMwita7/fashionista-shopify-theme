6ac38331fb572598f85063b466d1e24cab366a41
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-15 20:37:36 +0300

    log splides length

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,6 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     let splides = document.getElementsByClassName("splide__slide");
 
+    console.log("splides length: ", splides.length)
+
     var splide = new Splide( '.featured-collection-splide', {
         arrows: splides.length > 4,
         rewind: true,
