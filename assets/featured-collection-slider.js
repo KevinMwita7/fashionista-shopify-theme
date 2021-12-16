8a0256285aacf5eba62adec92e07780dfc4194c0
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-16 07:14:34 +0300

    show 3 slides on medium screens

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -2,9 +2,9 @@ document.addEventListener( 'DOMContentLoaded', function() {
     let splides = document.getElementsByClassName("featured-collection-slide");
 
     var splide = new Splide( '.featured-collection-splide', {
-        arrows: (screen.width >= 768 && splides.length > 4) || (screen.width < 768 && splides.length > 2),
+        arrows: (screen.width < 750 && splides.length > 2) || (screen.width >= 750 && screen.width < 990 && splides.length > 3) || (screen.width >= 990 && splides.length > 4),
         rewind: true,
-        perPage: screen.width >= 768 && splides.length > 3 ? 4 : 2
+        perPage:  screen.width >= 750 && screen.width < 990 && splides.length > 2 ? 3 : screen.width >= 990 && splides.length > 3 ? 4 : 2
     } );
     splide.mount();
 });
