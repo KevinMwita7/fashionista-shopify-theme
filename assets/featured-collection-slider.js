48fbf2f815df8c2f1b23032ecf5c25f504e48556
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-18 14:12:30 +0300

    added slides gutters

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,14 +1,18 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var swiper = new Swiper("#featured-collection-slider", {
+        pagination: true,
         breakpoints: {
             320: {
-                slidesPerView: 2
+                slidesPerView: 2,
+                spaceBetween: 20
             },
             768: {
-                slidesPerView: 3
+                slidesPerView: 3,
+                spaceBetween: 30
             },
             1024: {
-                slidesPerView: 5
+                slidesPerView: 5,
+                spaceBetween: 50
             }
         }
     });
