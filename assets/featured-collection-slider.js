f37083fe43e0effbdf43bbf152c578cb04249752
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-18 14:09:43 +0300

    added featured-collection-sliders

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,22 +1,15 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-    let splides = document.getElementsByClassName("featured-collection-slide");
-
-    var splide = new Splide( '.featured-collection-splide', {
-        arrows: (screen.width >= 768 && splides.length > 4) || (screen.width < 768 && splides.length > 2),
-        rewind: true,
-        perPage: 5
-        /**
-        arrows: (screen.width < 750 && splides.length > 2) || (screen.width >= 750 && screen.width < 990 && splides.length > 3) || (screen.width >= 990 && splides.length > 4),
-        rewind: true,
-        perPage:  screen.width >= 750 && screen.width < 990 && splides.length > 2 ? 3 : screen.width >= 990 && splides.length > 3 ? 4 : 2
-         */
-    } );
-
-    splide.on("arrows:mounted", function (prev, next) {
-        prev.style.top = "-9%";
-        prev.style.left = "90%";
-        next.style.top = "-9%";
+    var swiper = new Swiper("#featured-collection-slider", {
+        breakpoints: {
+            320: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 3
+            },
+            1024: {
+                slidesPerView: 5
+            }
+        }
     });
-
-    splide.mount();
 });
