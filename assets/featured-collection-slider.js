edf493abcaaf07bcff117c15291f204eed73b3db
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-18 14:16:48 +0300

    same slide margin on each breakpoint, add bullets

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,18 +1,19 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var swiper = new Swiper("#featured-collection-slider", {
-        pagination: true,
+        pagination: {
+            el: ".featured-collection-pagination",
+            clickable: true
+        },
+        spaceBetween: 20,
         breakpoints: {
             320: {
                 slidesPerView: 2,
-                spaceBetween: 20
             },
             768: {
                 slidesPerView: 3,
-                spaceBetween: 30
             },
             1024: {
                 slidesPerView: 5,
-                spaceBetween: 50
             }
         }
     });
