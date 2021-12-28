86430df0800234460f50b4d03a4f7fcd5b46bcd5
KevinMwita7 <kevinmwita7@gmail.com>
2021-12-28 14:42:12 +0300

    update banner-slider.js

--- assets/banner-slider.js
+++ assets/banner-slider.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
         
     document.addEventListener("shopify:block:select", function(event) {
-      console.log(event.detail);
+      console.log(event);
       swiper.slideTo(+event.target.dataset.slideIndex);
     });
   }
