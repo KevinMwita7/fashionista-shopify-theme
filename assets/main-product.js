32171768e0b3436b8e808938b384a853f760168e
KevinMwita7 <kevinmwita7@gmail.com>
2022-01-19 09:42:16 +0300

    added product image swiper and initiate swiper

---
+++ assets/main-product.js
@@ -0,0 +1,11 @@
+var swiper2 = new Swiper(".mySwiper2", {
+    // loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    /*thumbs: {
+        swiper: swiper,
+    },*/
+});
