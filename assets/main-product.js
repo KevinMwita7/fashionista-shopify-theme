5073ae5527e8b5259f065f53e3387080d9b157cd
KevinMwita7 <kevinmwita7@gmail.com>
2022-01-23 15:38:31 +0300

    set thumb image height

--- assets/main-product.js
+++ assets/main-product.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     // loop: true,
     spaceBetween: 10,
     slidesPerView: "auto",
+    centeredSlides: true,
     freeMode: true,
     watchSlidesProgress: true,
     direction: "vertical"
