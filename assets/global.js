4904056ccfec88b47704871ebe934f300202d26f
KevinMwita7 <kevinmwita7@gmail.com>
2022-01-30 12:07:51 +0300

    remove slidesPerview option

--- assets/global.js
+++ assets/global.js
@@ -608,8 +608,8 @@ class VariantSelects extends HTMLElement {
     const newMediaModal = modalContent.querySelector( `[data-media-id="${this.currentVariant.featured_media.id}"]`);
     const parent = newMedia.parentElement;
     if (parent.firstChild == newMedia) return;
-    /*modalContent.prepend(newMediaModal);
-    parent.prepend(newMedia);*/
+    modalContent.prepend(newMediaModal);
+    parent.prepend(newMedia);
     
     let slideTo = 0;
 
@@ -625,8 +625,6 @@ class VariantSelects extends HTMLElement {
     imageSwiper.update();
     thumbSwiper.slideTo(slideTo);
     thumbSwiper.update();
-    /*thumbSwiper.slideTo(slideTo);
-    thumbSwiper.update();*/
 
     this.stickyHeader = this.stickyHeader || document.querySelector('sticky-header');
     if(this.stickyHeader) {
