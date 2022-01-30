365ace84884af95777ca38553c7b05b350670a02
KevinMwita7 <kevinmwita7@gmail.com>
2022-01-30 19:11:55 +0300

    reverted changes

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,42 +1,46 @@
-let featuredMedia = document.querySelector('[data-is-featured-media="true"]');
-
-document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 10,
-        freeMode: true,
-        watchSlidesProgress: true,
-        direction: "horizontal",
-        slidesPerView: 6,
-        centerInsufficientSlides: true,
-        breakpoints: {
-           768: {
-               direction: "vertical"
-           } 
-        }
-    });
-    console.log(featuredMedia.attributes.getNamedItem("aria-label"))
-    var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 10,
-        initialSlide: +featuredMedia.attributes.getNamedItem("aria-label").value[0] - 1,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            type: "progressbar",
-        },
-        breakpoints: {
-            768: {
-                pagination: {
-                    // Set non existent class in order to hide pagination
-                    el: ".ix2pzra7iz",
-                    type: "progressbar"
-                }
-            }
-        },
-        thumbs: {
-            swiper: swiper,
-        },
-    });
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesProgress: true,
+    direction: "horizontal",
+    slidesPerView: 6,
+    centerInsufficientSlides: true,
+    breakpoints: {
+       768: {
+           direction: "vertical"
+       } 
+    }
 });
+
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    breakpoints: {
+        768: {
+            pagination: {
+                // Set non existent class in order to hide pagination
+                el: ".ix2pzra7iz",
+                type: "progressbar"
+            }
+        }
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+let featuredMedia = document.querySelector('[data-is-featured-media="true"]');
+for(let i = 0; i < featuredMedia.attributes.length; ++i) {
+    if(featuredMedia.attributes[i].name === "aria-label") {
+      swiper2.slideTo(+featuredMedia.attributes[i].value[0] - 1);
+      break;
+    }
+}
+console.log(featuredMedia.attributes.getNamedItem("aria-label"))
