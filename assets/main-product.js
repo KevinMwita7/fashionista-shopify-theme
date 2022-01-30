fbc2217193dfcb0b1a48e4970e30e47cf99399ae
KevinMwita7 <kevinmwita7@gmail.com>
2022-01-30 18:54:03 +0300

    dynamic initial slide

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,52 +1,43 @@
-let initialSlide = 0;
-
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    freeMode: true,
-    watchSlidesProgress: true,
-    direction: "horizontal",
-    slidesPerView: 6,
-    centerInsufficientSlides: true,
-    breakpoints: {
-       768: {
-           direction: "vertical"
-       } 
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     let featuredMedia = document.querySelector('[data-is-featured-media="true"]');
-    for(let i = 0; i < featuredMedia.attributes.length; ++i) {
-        if(featuredMedia.attributes[i].name === "aria-label") {
-          // swiper2.slideTo(+featuredMedia.attributes[i].value[0] - 1);
-          initialSlide = +featuredMedia.attributes[i].value[0] - 1;
-          break;
-        }
-    }
-    console.log(featuredMedia.attributes.getNamedItem("aria-label"))
-});
+    let initialSlide = +featuredMedia.attributes.getNamedItem("aria-label").value[0] - 1;
 
-var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    initialSlide: 5,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-    },
-    breakpoints: {
-        768: {
-            pagination: {
-                // Set non existent class in order to hide pagination
-                el: ".ix2pzra7iz",
-                type: "progressbar"
-            }
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        freeMode: true,
+        watchSlidesProgress: true,
+        direction: "horizontal",
+        slidesPerView: 6,
+        centerInsufficientSlides: true,
+        breakpoints: {
+           768: {
+               direction: "vertical"
+           } 
         }
-    },
-    thumbs: {
-        swiper: swiper,
-    },
+    });
+    
+    var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        initialSlide: initialSlide,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        breakpoints: {
+            768: {
+                pagination: {
+                    // Set non existent class in order to hide pagination
+                    el: ".ix2pzra7iz",
+                    type: "progressbar"
+                }
+            }
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
 });
