f0749f466507e2b6e9507d8670b2764ae9a501b2
KevinMwita7 <kevinmwita7@gmail.com>
2022-02-02 21:40:52 +0300

    dont initialize slider if images count less than 1

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,45 +1,49 @@
 // let featuredMedia = document.querySelector('[data-is-featured-media="true"]');
-let featuredMedia = document.querySelector("[id^=variant-featured-media-]").id;
+let productImages = document.querySelectorAll(".mySwiper2 .swiper-slide");
 
-// Initialize swiper and its thumbnails
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    freeMode: true,
-    watchSlidesProgress: true,
-    direction: "horizontal",
-    slidesPerView: 6,
-    centerInsufficientSlides: true,
-    breakpoints: {
-       768: {
-           direction: "vertical"
-       } 
-    }
-});
+if(productImages.length) {
+    let featuredMedia = document.querySelector("[id^=variant-featured-media-]").id;
 
-var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    initialSlide: +featuredMedia[featuredMedia.length - 1] - 1,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-    },
-    breakpoints: {
-        768: {
-            pagination: {
-                // Set non existent class in order to hide pagination
-                el: ".ix2pzra7iz",
-                type: "progressbar"
-            }
+    // Initialize swiper and its thumbnails
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        freeMode: true,
+        watchSlidesProgress: true,
+        direction: "horizontal",
+        slidesPerView: 6,
+        centerInsufficientSlides: true,
+        breakpoints: {
+           768: {
+               direction: "vertical"
+           } 
         }
-    },
-    thumbs: {
-        swiper: swiper,
-    },
-});
+    });
+    
+    var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        initialSlide: +featuredMedia[featuredMedia.length - 1] - 1,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        breakpoints: {
+            768: {
+                pagination: {
+                    // Set non existent class in order to hide pagination
+                    el: ".ix2pzra7iz",
+                    type: "progressbar"
+                }
+            }
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+}
 
 /*for(let i = 0; i < featuredMedia.attributes.length; ++i) {
     if(featuredMedia.attributes[i].name === "aria-label") {
@@ -49,7 +53,6 @@ var swiper2 = new Swiper(".mySwiper2", {
 }*/
 
 // Initialize zoom image
-let productImages = document.querySelectorAll(".mySwiper2 .swiper-slide");
 for(let i = 0; i < productImages.length; ++i) {
     new ImageZoom(productImages[i], {
         fillContainer: true,
